459295077cda61954d79762762fb38e0738367b7
D.A Jalloh <info@codewithjalloh.com>
2016-06-28 21:53:51 +0100

    Initial Commit

---
+++ ShareViaFacebookAndTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareViaFacebookAndTwitter
+//
+//  Created by wealthyjalloh on 28/06/2016.
+//  Copyright © 2016 CWJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
